54822d6524081b4c29046c5b2b033fd57ab2832f
Axel Petitjean <axelpeti@c1r3p7.42perpignan.fr>
2024-12-09 11:13:35 +0100

    fonctionnel

--- get_next_line.c
+++ get_next_line.c
@@ -5,52 +5,110 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: axelpeti <axelpeti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/27 10:52:49 by axelpeti          #+#    #+#             */
-/*   Updated: 2024/11/28 14:05:05 by axelpeti         ###   ########.fr       */
+/*   Created: 2024/12/07 10:16:37 by axelpeti          #+#    #+#             */
+/*   Updated: 2024/12/09 11:11:43 by axelpeti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static char	*stach(char *buf)
+char	*readline(int fd, char *stash)
 {
-	char		*bufcpy;
-	char		*line;
-	size_t		size;
+	char	*buf;
+	int		bytes;
 
-	bufcpy = ft_strdup(buf);
-	if (ft_strchr(bufcpy))
+	buf = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
+	if (!buf)
+		return (NULL);
+	bytes = 1;
+	while (bytes > 0 && ft_strchr(stash, '\n') == -1)
 	{
-		line = ft_strjoin(bufcpy);
-		bufcpy = 
+		bytes = read(fd, buf, BUFFER_SIZE);
+		if (bytes == -1)
+			return (free(buf), free(stash), stash = NULL, NULL);
+		buf[bytes] = '\0';
+		if (!stash)
+			stash = ft_strdup(buf);
+		else
+			stash = ft_strjoin(stash, buf);
 	}
-	else
-		get_next_line;
+	return (free (buf), stash);
+}
+
+char	*recover_beforen(char *stash)
+{
+	int		i;
+	int		pos;
+	char	*line;
+
+	i = 0;
+	pos = ft_strchr(stash, '\n');
+	if (pos == -1)
+		pos = ft_strlen(stash) + 1;
+	line = ft_calloc(pos + 2, sizeof(char));
+	if (!line)
+		return (free(stash), stash = NULL, NULL);
+	while (stash[i] && i <= pos)
+	{
+		line[i] = stash[i];
+		i++;
+	}
+	return (line);
+}
+
+char	*recover_aftern(char *stash)
+{
+	int		i;
+	int		j;
+	int		pos;
+	char	*newstash;
+
+	j = 0;
+	i = ft_strchr(stash, '\n');
+	if (i == -1)
+		return (free(stash), stash = NULL, NULL);
+	pos = ft_strlen(stash);
+	i++;
+	newstash = ft_calloc(pos - i + 1, sizeof(char));
+	if (!newstash)
+		return (free(stash), stash = NULL, NULL);
+	while (stash[i])
+	{
+		newstash[j] = stash[i];
+		j++;
+		i++;
+	}
+	free(stash);
+	stash = NULL;
+	return (newstash);
 }
 
 char	*get_next_line(int fd)
 {
-	char	*buffer;
-	int 	bytes;
-	char 	*line;
+	char		*line;
+	static char	*stash = NULL;
 
-	buffer = (char *)malloc(BUFFER_SIZE * sizeof(char));
-	if (!buffer)
+	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	line = (char *) malloc(ft_strlen() * sizeof (char *));
-	bytes = read(fd, buffer, BUFFER_SIZE);
-	if (bytes == -1)
+	stash = readline(fd, stash);
+	if (!stash)
 		return (NULL);
-	while (bytes != 0)
-	{
-		line = stach(buffer);
-	}
+	if (stash[0] == '\0')
+		return (free(stash), stash = NULL, NULL);
+	line = recover_beforen(stash);
+	stash = recover_aftern(stash);
 	return (line);
 }
-int main ()
+/* 
+int main(void)
 {
-	int fd;
-	fd = open ("test.txt", O_RDONLY);
-	get_next_line(fd);
+	char	*line;
+	int fd = open("41_no_nl", O_RDONLY);
+	line = (get_next_line(fd));
+	printf("line = %s", line);
+	free(line);
+	line = (get_next_line(fd));
+	printf("line = %s", line);
+	free(line);
 	close(fd);
-}
+} */
--- get_next_line.h
+++ get_next_line.h
@@ -5,36 +5,32 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: axelpeti <axelpeti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/27 10:53:54 by axelpeti          #+#    #+#             */
-/*   Updated: 2024/11/28 13:22:37 by axelpeti         ###   ########.fr       */
+/*   Created: 2024/12/07 10:19:55 by axelpeti          #+#    #+#             */
+/*   Updated: 2024/12/09 11:08:56 by axelpeti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# ifndef GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+# include <unistd.h>
+# include <stddef.h>
+# include <stdio.h>
 # include <stdlib.h>
 # include <fcntl.h>
-# include <unistd.h>
 
-#ifndef BUFFER_SIZE		//macro BUFFER_SIZE
-# define BUFFER_SIZE 100  // Valeur par défaut, tu peux changer cette taille à la compilation
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 50
+# endif
 
-char	*ft_strdup(const char *s);
-size_t	ft_strlen(char *str);
+int		ft_strlen(char *s);
+void	*ft_calloc(int nmemb, int size);
+char	*ft_strjoin(char *s1, char *s2);
+int		ft_strchr(char *s, int c);
+char	*ft_strdup(char *s);
+char	*readline(int fd, char *stach);
+char	*recoveraftern(char *stach);
+char	*recoverbeforen(char *line);
 char	*get_next_line(int fd);
-char	*ft_strchr(const char *s);
-char	*ft_strjoin(char const *s1);
+
 #endif
-
-
-
-/* a chaque fois que jouvre un fichier j'envoie un pointeur, je doit verifier si ce pointeur n'est pas nul pour savoir
-si louverture a fonctionnera la fin il faut penser a le fermer
-
-mon but :
-ouvrir dans ma fonction principal avec open, une fois ouverte verifier le pointeur, envoyer a une fonction auxiliaire qui permettra de lire une ligne (jusqu'au \n) ou jusqu'a la limite du buffer
-
-l'envoyer dans le buffer puis a une fonction pour quel recupere dans le buffer les charractere a envoyer dans le terminal free le buffer et retourner sur la lecture jusqua la suite d'une ligne jusqu'au \n et ceux jusqu'a la fin du fichier
-
-fonction : strlen, strcopy ou strdup car besoin malloc mais modifier pour free le buffer sauf si on free le buffer apres lappel de fonction strdup, */
